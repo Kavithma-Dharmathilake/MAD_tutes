d9f8ae6f1463fd7d6294d88bb3db2d54a73efd92
Kavithma Dharmathilake <117156528+Kavithma-Dharmathilake@users.noreply.github.com>
2023-06-02 21:13:58 +0530

    Tutoial 5 UI

---
+++ app/src/main/java/com/example/mad_tute/adapters/TodoAdapter.kt
@@ -0,0 +1,19 @@
+package com.example.mad_tute.adapters
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+
+class TodoAdapter:RecyclerView.Adapter<ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
