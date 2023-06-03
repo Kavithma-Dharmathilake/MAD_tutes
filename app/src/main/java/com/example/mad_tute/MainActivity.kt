c7d02b99e8cfc46baf1155d00ee914e3e54fc27e
Kavithma Dharmathilake <117156528+Kavithma-Dharmathilake@users.noreply.github.com>
2023-06-03 11:28:34 +0530

    Final commit

--- app/src/main/java/com/example/mad_tute/MainActivity.kt
+++ app/src/main/java/com/example/mad_tute/MainActivity.kt
@@ -2,26 +2,75 @@ package com.example.mad_tute
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mad_tute.adapters.TodoAdapter
+import com.example.mad_tute.database.TodoDatabase
+import com.example.mad_tute.database.entities.Todo
+import com.example.mad_tute.database.repositories.TodoRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val recyclerView:RecyclerView = findViewById(R.id.rvTodoList)
+        val repository = TodoRepository(TodoDatabase.getInstance(this))
+        val recyclerView: RecyclerView = findViewById(R.id.rvTodoList)
         val adapter = TodoAdapter()
+        val ui = this
         recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = LinearLayoutManager(ui)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val data = repository.getAllTodos()
+            adapter.setData(data, ui)
+        }
+
+        val btnAddTodo = findViewById<Button>(R.id.btnAddTodo)
+        btnAddTodo.setOnClickListener{
+            displayDialog(repository,adapter)
+        }
 
     }
 
 
 
+    fun displayDialog(repository: TodoRepository, adapter: TodoAdapter) {
 
+        val builder = AlertDialog.Builder(this)
 
+        builder.setTitle("Enter New Todo item: ")
+        builder.setMessage("Enter the todo item Below: ")
 
+        val input = EditText(this)
+        input.inputType = InputType.TYPE_CLASS_TEXT
+        builder.setView(input)
+
+        builder.setPositiveButton("OK") { dialog, which ->
+
+            val item = input.text.toString()
+            CoroutineScope(Dispatchers.IO).launch {
+                repository.insert(Todo(item))
+                val data = repository.getAllTodos()
+                runOnUiThread {
+                    adapter.setData(data, this@MainActivity)
+                }
+            }
+        }
+
+        builder.setNegativeButton("Cancel") { dialog, which ->
+            dialog.cancel()
+        }
+
+        val alertDialog = builder.create()
+        alertDialog.show()
+    }
 
 }
