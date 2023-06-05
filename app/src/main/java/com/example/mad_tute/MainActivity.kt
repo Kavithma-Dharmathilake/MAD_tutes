31b2329b9a36c71d78c8cabae9974e197235bc2d
Kavithma Dharmathilake <117156528+Kavithma-Dharmathilake@users.noreply.github.com>
2023-06-05 08:00:18 +0530

    App using fragments

--- app/src/main/java/com/example/mad_tute/MainActivity.kt
+++ app/src/main/java/com/example/mad_tute/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.mad_tute
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val imgHome: ImageView = findViewById(R.id.imgHome)
+        val imgUser: ImageView = findViewById(R.id.imgUser)
+        val fragmentHome = HomeFragment()
+        val fragmentUser = UserFragment()
+
+        imgUser.setOnClickListener {
+            imgHome.setImageResource(R.drawable.home)
+            imgUser.setImageResource(R.drawable.active_user)
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragmentContainerView, fragmentUser)
+                commit()
+            }
+        }
+
+        imgHome.setOnClickListener {
+            imgHome.setImageResource(R.drawable.active_home)
+            imgUser.setImageResource(R.drawable.user)
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragmentContainerView, fragmentHome)
+                commit()
+            }
+        }
     }
 }
+
