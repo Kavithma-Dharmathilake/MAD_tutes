62a5bc3f51c58d5f0e40664e1c03c78c6f2d63b0
Kavithma Dharmathilake <117156528+Kavithma-Dharmathilake@users.noreply.github.com>
2023-06-05 09:27:39 +0530

    Instrumental test

--- app/src/main/java/com/example/mad_tute/MainActivity.kt
+++ app/src/main/java/com/example/mad_tute/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.mad_tute
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.widget.ButtonBarLayout
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var button : Button
+    lateinit var text : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.btnClick)
+        text = findViewById(R.id.tvMessage)
+
+        button.setOnClickListener {
+            text.text = "Hello, User!"
+        }
+
+
     }
+
+
 }
