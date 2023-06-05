1ec7af4927e4370b14486dbb02181be369b7043d
Kavithma Dharmathilake <117156528+Kavithma-Dharmathilake@users.noreply.github.com>
2023-06-05 08:21:00 +0530

    App using BroadCast Recivers

--- app/src/main/java/com/example/mad_tute/MainActivity.kt
+++ app/src/main/java/com/example/mad_tute/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.mad_tute
 
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var myBroadcastReceiver: MyReceiver
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        myBroadcastReceiver = MyReceiver()
+
+        val filter = IntentFilter("com.example.mad_tute.MY_CUSTOM_ACTION")
+        val listenToBroadcastsFromOtherApps = false
+        val receiverFlags = if (listenToBroadcastsFromOtherApps) {
+            ContextCompat.RECEIVER_EXPORTED
+        } else {
+            ContextCompat.RECEIVER_NOT_EXPORTED
+        }
+        ContextCompat.registerReceiver(this, myBroadcastReceiver,filter,receiverFlags)
+        val systemFilter = IntentFilter()
+        systemFilter.addAction(Intent.ACTION_SCREEN_ON)
+        systemFilter.addAction(Intent.ACTION_SCREEN_OFF)
+        systemFilter.addAction(Intent.ACTION_BATTERY_LOW)
+        ContextCompat.registerReceiver(this, myBroadcastReceiver,systemFilter,receiverFlags)
+        GlobalScope.launch {
+            delay(5000)
+            val intent = Intent()
+            intent.action = "com.example.mad_tute.MY_CUSTOM_ACTION"
+            sendBroadcast(intent)
+
+
+        }
     }
 }
